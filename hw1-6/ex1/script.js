680263c1c741ac86f908aee38ab025dc2b59d018
Sasha Skorokhod <ganag.bomzh@mail.ru>
2023-04-04 21:22:21 +0300

    1-6

---
+++ hw1-6/ex1/script.js
@@ -0,0 +1,20 @@
+/* 2.1
+let i = 'Я Javs script!';
+alert(i);
+console.log(i);*/
+
+/*let admin, name;
+name = 'john';
+admin = name;
+alert(admin)*/
+
+/* 1.5 
+hello 1
+hello name
+hello ilya
+*/
+
+/* 1.6
+let name = prompt('Сколько тебе лет?', 18);
+alert(name);
+*/
